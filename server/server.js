a5ca8056e31a1447811fb8b555da21a42be5a807
Hunter Houston <hmhouston7@gmail.com>
2017-04-17 17:53:33 -0500

    change root in server.js from const to let

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ app.use(function (req, res, next) {
 });
 
 app.get('/test', function(req, res) {
-  const root = req.query.root
+  let root = req.query.root;
   // let root = '/stornext/snfs1';
   console.log('root: ', root);
   console.log('root is of type: ', (typeof root));
