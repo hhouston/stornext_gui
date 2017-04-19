1ca9a70f21946649abbf74c41832c44c75a01811
Hunter Houston <hmhouston7@gmail.com>
2017-04-19 14:42:56 -0500

    adds a port listen on 23939 that uses the node module cors. switch the app.use line to occur before the app.listen in server.js. going to test the react app on the load balancer with the node server on the LINUX server.

--- server/server.js
+++ server/server.js
@@ -1,24 +1,62 @@
 var express = require('express');
 var app = express();
+var cors_app = express();
+
 var fs = require("fs");
 var path = require("path");
+var cors = require('cors')
 
 var ip = process.argv[2];
 var port = process.argv[3]
 
 
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET');
+    res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Origin, Content-Type');
+    next();
+});
+
 var server = app.listen(port, ip, () => {
   console.log('server listening at http://%s:%s', ip, port);
 });
 
-app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET');
-    // res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Origin, Content-Type');
-    next();
+app.use(cors());
+
+var server2 = cors_app.listen(23939, ip, () => {
+  console.log('cors module server listening at http://%s:%s', ip, 23939);
 });
 
 app.get('/test', function(req, res) {
+  let root = req.query.root;
+  // let root = '/stornext/snfs1';
+  console.log('root: ', root);
+  console.log('root is of type: ', (typeof root));
+  console.log('req headers: ', req.headers);
+
+  fs.readdir(root, (err, files) => {
+    if (err) {
+      console.log("Error: " + err);
+    } else {
+      files.forEach((file, idx) => {
+        console.log(idx + ":" + file);
+      });
+    }
+
+    res.writeHead(200, {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*"
+    });
+
+    var json = JSON.stringify({
+      root,
+      files
+    });
+    res.end(json);
+  })
+});
+
+cors_app.get('/test', function(req, res) {
   let root = req.query.root;
   // let root = '/stornext/snfs1';
   console.log('root: ', root);
