9941ff93d81fc2a2f3dac94186429048cbe8fc36
Hunter Houston <hmhouston7@gmail.com>
2017-04-19 15:03:50 -0500

    change the url to be local host (testing if we run node and react on same private server)

--- src/App.js
+++ src/App.js
@@ -43,9 +43,9 @@ class App extends Component {
   fetchFiles(root) {
     var options = {
       // url: 'http://requestb.in/1m3h8fe1',
-      // url: 'http://localhost:8080/test',
+      url: 'http://localhost:8080/test',
       // url: 'http://136.179.6.56:8080/test',
-      url: 'http://172.16.3.51:23939/test',
+      // url: 'http://172.16.3.51:23939/test',
       // url: 'http://172.16.3.51:8080/test',
       // headers: {
       //   'Content-Type': 'request',
