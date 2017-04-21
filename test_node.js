9ead1bf80c783129bc55ba96b1f1143099573f6f
Hunter Houston <hmhouston7@gmail.com>
2017-04-21 15:14:15 -0700

    adds test_node to make request from MDC

--- test_node.js
+++ test_node.js
@@ -2,13 +2,15 @@
 
 var request = require('request');
 
+let root = "/";
+
 var options = {
   // url: 'http://requestb.in/19qhfq01',
-  // url: 'http://localhost:8080/test',
+  url: 'http://localhost:8080/test',
   // url: 'http://localhost:23939/test',
   // url: 'http://136.179.6.56:8080/test',
   // url: 'http://172.16.3.51:23939/test',
-  url: 'http://172.16.3.51:8080/test',
+  // url: 'http://172.16.3.51:8080/test',
   // url: 'http://google.com/',
   headers: {
     'Content-Type': 'request',
@@ -45,19 +47,20 @@ var options = {
 //   console.log('body:', body); // Print the HTML for the Google homepage.
 // });
 
-request(options, (err, res, body) => {
-  console.log('request made res: ', res);
-  if (!err && res.statusCode === 200) {
-    var json = JSON.parse(body);
-    console.log("body: " + body)
-    this.setState ({
-      root: root,
-      files: json.files
-    });
+const req = () => {
+  request(options, (err, res, body) => {
+    console.log('request made res: ', res);
+    if (!err && res.statusCode === 200) {
+      var json = JSON.parse(body);
+      console.log("body: " + body)
 
-    console.log(json.root + " root");
-    console.log(json.files + " files");
-  } else {
-    console.log('error: ', err);
-  }
-});
+      console.log(json.root + " root");
+      console.log(json.files + " files");
+    } else {
+      console.log('error: ', err);
+    }
+  });
+}
+
+
+req();
