ab7fdf2d315af72d4e3ad5aef50932d29f14ad69
Hunter Houston <hmhouston7@gmail.com>
2017-04-23 21:04:54 -0700

    adds headers to the fetchFiles ajax request

--- supernova-master/src/actions/home.js
+++ supernova-master/src/actions/home.js
@@ -1,6 +1,13 @@
 export function fetchFiles(root) {
   return (
     $.ajax({
+      headers: {
+        'Content-Type': 'request',
+        'Access-Control-Allow-Origin': '*'
+      },
+      qs: {
+        root: root
+      },
       method: "GET",
       url: 'http://172.16.3.51:8080/test?root=%2F'
     })
