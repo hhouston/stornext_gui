c7106d1a57af520f33649ec40e83cf508db34b68
Hunter Houston <hmhouston7@gmail.com>
2017-04-23 22:10:31 -0700

    change for testing on single production server

--- supernova-master/src/actions/home.js
+++ supernova-master/src/actions/home.js
@@ -1,16 +1,9 @@
 export function fetchFiles(root) {
   return (
     $.ajax({
-      contentType: 'application/json',
-      headers: {
-        'Access-Control-Allow-Origin': '*'
-      },
-      qs: {
-        root: root
-      },
       method: "GET",
-      // url: 'http://172.16.3.51:8080/test'
-      url: 'http://requestb.in/1f87ot71'
+      url: 'http://localhost:9000/test?root=%2F'
+      // url: 'http://requestb.in/1f87ot71'
     })
   );
 }
